b1d51929c57b3bf6063b49af383f826bd651322c
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-16 20:23:07 -0300

    resolucion ej 03

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,13 @@
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+
+
 function mostrar()
 {
 	var nombreIngresado;
 	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	alert("El nombre ingresado es "+nombreIngresado);
 
 }
 
