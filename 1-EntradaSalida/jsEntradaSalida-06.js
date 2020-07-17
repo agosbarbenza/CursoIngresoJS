2bc0ee646486dbd48f83f8d6ce8952b48d030660
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-17 17:01:31 -0300

    resueltos

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -2,9 +2,10 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
+
 function sumar()
 {
 	var num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
 	var num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
-	alert("El resultado es "+ (num1+num2));
+	alert("La suma es "+ (num1+num2));
 }
