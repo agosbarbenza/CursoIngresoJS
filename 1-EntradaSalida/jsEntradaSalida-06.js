10bc022ec8054a2e9f871ee368404cfebc1e5251
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-16 22:32:36 -0300

    resolucion ej 05

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,8 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	alert("El resultado es "+ (num1+num2));
 }
 
