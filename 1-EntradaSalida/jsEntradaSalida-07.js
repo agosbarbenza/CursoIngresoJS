1cd74ed96e65e432c86574a2361dfbfef06921d8
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-17 16:30:30 -0300

    resolucion ej 07

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,37 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	alert("ok");	
+	var numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	var result = parseInt(numero1 + numero2);
+	alert("La suma es "+result);	
 }
 
 function restar()
 {
+	var numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	var result = parseInt(numero1-numero2);
+	alert("La resta es " + result);
 	
 }
 
 function multiplicar()
 { 
-	
+	var numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	var result = parseInt(numero1*numero2);
+	alert("La multiplicación es "+result);
 }
 
 function dividir()
 {
-	
+	var numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	var numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	var result = parseInt(numero1/numero2);
+	alert("La división es "+result);
 }
 
