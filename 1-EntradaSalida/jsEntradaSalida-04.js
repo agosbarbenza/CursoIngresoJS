e3d96c1ba34ef7bddd3faada8077b19dab7edd68
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-16 21:49:16 -0300

    resolucion ej 04

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,12 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
+
+
 function mostrar()
 {
-	alert("ok");
+	var dato = prompt("Ingrese su nombre");
+	document.getElementById("txtIdNombre").value = dato
 }
 
