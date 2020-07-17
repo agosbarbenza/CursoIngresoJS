5fa502300fcffc760118892ec3cbcd87345049fe
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-17 20:01:57 -0300

    codigo más resumido ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var importe = parseInt(document.getElementById("txtIdImporte").value);
+	document.getElementById("txtIdResultado").value = importe - ((importe*15)/100);
+
 }
