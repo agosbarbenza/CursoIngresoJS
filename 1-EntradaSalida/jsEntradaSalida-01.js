3edf96811a6779016ddbd8fe01cab2e735d9e0c4
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-17 22:01:13 -0300

    resolucion ej 01

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
+
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Agostina Barbenza.");
+	alert("Esto funciona de maravilla");
 }
 
-=======
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("Agostina");
-}
-
->>>>>>> 9aad24a30d9fc626446ad77633fcfa5c474c3135
