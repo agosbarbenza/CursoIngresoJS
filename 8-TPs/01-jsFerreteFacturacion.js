9bc2f0728f293696f4129897f26713c1831e2861
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-22 19:35:33 -0300

    resolucion tp 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,30 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    alert("La suma es "+(precioUno+precioDos+precioTres))
 	
 }
 function Promedio () 
 {
-	
+	var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    alert("El promedio de los números es "+(precioUno+precioDos+precioTres)/3);
 }
 function PrecioFinal () 
 {
+    var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    var suma = precioUno+precioDos+precioTres;
+    var conIva = (suma+(suma * 0.21))
+
+    alert("El precio final es "+ conIva);
 	
 }
