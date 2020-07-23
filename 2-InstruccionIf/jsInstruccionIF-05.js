780b1485211dfd221245e6077df0c7e206f1ee9d
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-22 22:10:17 -0300

    resolucion ej 5

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,9 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (!(edad>=13 && edad<=17)){
+		alert("No es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
