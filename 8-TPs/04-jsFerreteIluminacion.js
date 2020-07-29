69af120a085008bbd8084810cdfe8f02dbccaf2a
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-07-29 10:56:29 -0300

    resolucion tp 4

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -9,49 +9,68 @@ E.	Si el importe final con descuento suma más de $120 se debe sumar un 10% de i
 
  */
 
-function CalcularPrecio () 
-{
+function CalcularPrecio() {
     let qLamp = parseInt(document.getElementById("txtIdCantidad").value);//si o si la cantidad de lamparitas tiene que ser un numero entero.
     let precioUnitario = 35; //conviene guardar el precio unitario en una variable para, en el caso de que aumente o baje, no tener que cambiar el valor uno por uno en el código.
+    let precioTotal
     let precioDescuento = document.getElementById("txtIdprecioDescuento");
     let marca = document.getElementById("Marca").value;
 
-//cantidad de lamparas x precio unitario  = precio total.
+    //cantidad de lamparas x precio unitario  = precio total.
 
-//descuento: precio total - precio total * 0,50 
+    //descuento: precio total - precio total * 0,50 
 
-    if (qLamp>=6){
-        precioDescuento.value= "$"+parseFloat((precioUnitario*qLamp)-(((precioUnitario*qLamp)*50)/100)); 
-    }else{
-    switch (qLamp) {
-        case 1:
-        case 2:
-            precioDescuento.value = "$"+(precioUnitario*qLamp);
-            break;
-        case 3:
-            if(marca == "ArgentinaLuz"){
-               precioDescuento.value= "$"+parseFloat((precioUnitario*3)-((precioUnitario*3)*0.15));
-            }else if(marca == "FelipeLamparas"){
-                precioDescuento.value= "$"+parseFloat((precioUnitario*3)-(((precioUnitario*3)*0.10))); 
-            }else{
-                precioDescuento.value= "$"+parseFloat((precioUnitario*3)-((precioUnitario*3)*0.05));
-            }
-            break;
-        case 4:
-            if(marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
-                precioDescuento.value= "$"+parseFloat((precioUnitario*4)-(((precioUnitario*4)*0.25))); 
-            }else{
-                precioDescuento.value= "$"+parseFloat((precioUnitario*4)-(((precioUnitario*4)*0.20))); 
-            }
-            break;
-        case 5:
-            if(marca == "ArgentinaLuz"){
-                precioDescuento.value= "$"+parseFloat((precioUnitario*5)-(((precioUnitario*5)*0.40))); 
-            }else{
-                precioDescuento.value= "$"+parseFloat((precioUnitario*5)-(((precioUnitario*5)*0.30)));
-            }
-            break;
+
+    if (qLamp >= 6) {
+
+        precioTotal = parseFloat((precioUnitario * qLamp) - (((precioUnitario * qLamp) * 50) / 100));
+        precioDescuento.value = "$" + precioTotal;
+    } else {
+        switch (qLamp) {
+            case 1:
+            case 2:
+                precioTotal = (precioUnitario * qLamp);
+                precioDescuento.value = "$" + precioTotal;
+                break;
+            case 3:
+                if (marca == "ArgentinaLuz") {
+                    precioTotal = parseFloat((precioUnitario * 3) - ((precioUnitario * 3) * 0.15));
+                    precioDescuento.value = "$" + precioTotal;
+                } else if (marca == "FelipeLamparas") {
+                    precioTotal = parseFloat((precioUnitario * 3) - (((precioUnitario * 3) * 0.10)));
+                    precioDescuento.value = "$" + precioTotal;
+                } else {
+                    precioTotal = parseFloat((precioUnitario * 3) - ((precioUnitario * 3) * 0.05));
+                    precioDescuento.value = "$" + precioTotal;
+                }
+                break;
+            case 4:
+                if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+                    precioTotal = parseFloat((precioUnitario * 4) - (((precioUnitario * 4) * 0.25)));
+                    precioDescuento.value = "$" + precioTotal;
+                } else {
+                    precioTotal = parseFloat((precioUnitario * 4) - (((precioUnitario * 4) * 0.20)));
+                    precioDescuento.value = "$" + precioTotal;
+                }
+                break;
+            case 5:
+                if (marca == "ArgentinaLuz") {
+                    precioTotal = parseFloat((precioUnitario * 5) - (((precioUnitario * 5) * 0.40)));
+                    precioDescuento.value = "$" + precioTotal;
+                } else {
+                    precioTotal = parseFloat((precioUnitario * 5) - (((precioUnitario * 5) * 0.30)));
+
+                    precioDescuento.value = "$" + precioTotal;
+                }
+                break;
+        }
     }
-}
+
+    if (precioTotal >= 120) {
+        let iiBB = (precioTotal * 0.10);
+        precioTotal = precioTotal + iiBB;
+        precioDescuento.value = precioTotal;
+        alert("Usted pagó $" + iiBB + " de IIBB");
+    }
 
 }
