3c9e54ac95d2ac4866fc1619606ec5e018295a4f
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-06 20:55:11 -0300

    cambios

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -2,12 +2,44 @@
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{	// declarar variables
-	
+{
+	/////////////////////////METODO DE BANDERA////////////////////////////////
 	let numIngresado;
 	let numMax = 0;
 	let numMin = 0;
+	let bandera = 0;
+	let respuesta;
+
+	do{
+	numIngresado = parseInt(prompt("Ingrese un numero")); //pide un numero
+
+	if(bandera == 0){	//cuando el contador tome valor 1
+		numMax = numIngresado;  //asigna como max y min ese numero
+		numMin = numIngresado;  
+	}
+
+	bandera = 1;
+
+	if(numIngresado>numMax){ //cuando el contador sea mayor a 1, y el nuevo numero sea mayor al ingresado antes
+		numMax = numIngresado; //se asigna ese numero nuevo como maximo
+	}else if(numIngresado<numMin){ //si el nuevo numero ingresado es MENOR al numero anterior
+		numMin = numIngresado; //asigna como MINIMO a ese numero nuevo.
+	}
+	respuesta=prompt("desea continuar?");
+	}while(respuesta == "si");
+
+	document.getElementById("txtIdMaximo").value = numMax;
+	document.getElementById("txtIdMinimo").value = numMin;
+
+}//FIN DE LA FUNCIÓN
+
+///////////////////////////////////////MI FORMA///////////////////////////////
+
+	/*let numIngresado;
+	let numMax = 0;
+	let numMin = 0;
 	let contador = 0;
+	let respuesta;
 
 	do{
 	numIngresado = parseInt(prompt("Ingrese un numero")); //pide un numero
@@ -27,22 +59,30 @@ function mostrar()
 	}while(respuesta == "si");
 
 	document.getElementById("txtIdMaximo").value = numMax;
-	document.getElementById("txtIdMinimo").value = numMin;
+	document.getElementById("txtIdMinimo").value = numMin;*/
 
-}//FIN DE LA FUNCIÓN
+	///////////////////////////////////////FORMA DE LA PROFE//////////////////////
 
-/*var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
+	/*let max;
+	let min;
+	let num;
+	let resp;
+
+	resp = "si";
+	num = parseInt(prompt("Ingrese un número"));
+	max = num;
+	min = num;
+	resp = prompt("Desea ingresar otro numero? si/no");
+
+	while(resp == "si"){
+		num = parseInt(prompt("Ingrese un número"));
+		if(num>max){
+			max = num;
+		}else if(num<min){
+			min = num;
+		}
+		resp = prompt("Desea ingresar otro numero? si/no");
 		
-		respuesta=prompt("desea continuar?");
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;*/
+	console.log("El maximo es "+max+" y el minimo es "+min);*/
+
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -22,7 +22,7 @@ function mostrar() {
 	let cantidadPares = 0;
 	let promedioPositivos = 0;
 	let promedioNegativos = 0;
-	let diferencia ;
+	let diferencia;
 	let control1 = 0; //variable de control pares
 	let control2 = 0; //variable de control positivos
 	let control3 = 0; //variable de control negativos
@@ -43,7 +43,7 @@ function mostrar() {
 				control1++;
 				cantidadPares = control1;
 			}
-		} else if(numeroIngresado < 0) {
+		} else if (numeroIngresado < 0) {
 			control3++
 			sumaNegativos = sumaNegativos + numeroIngresado;
 			cantidadNegativos = control3;
@@ -63,7 +63,57 @@ function mostrar() {
 		respuesta = prompt("desea continuar?");
 	} while (respuesta == "si");
 
-	diferencia = sumaPositivos-sumaNegativos;
+	diferencia = sumaPositivos - sumaNegativos;
 
-	document.write("1 - La suma de los negativos es: " + sumaNegativos + "<br>" + "2 - La suma de los positivos es: " + sumaPositivos + "<br>" + "3 - La cantidad de positivos es: " + cantidadPositivos + "<br>" + "4 - La cantidad de negativos es: " + cantidadNegativos + "<br>" + "5 - La cantidad de ceros es: " + cantidadCeros + "<br>" + "6 - La cantidad de números pares es: " + cantidadPares + "<br>" + "7 - El promedio de positivos es: " + promedioPositivos + "<br>" + "8 - El promedio de negativos es: " + promedioNegativos+"<br>"+" 9 - La diferencia entre los positivos y los negativos es: "+diferencia);
+	document.write("1 - La suma de los negativos es: " + sumaNegativos + "<br>" + "2 - La suma de los positivos es: " + sumaPositivos + "<br>" + "3 - La cantidad de positivos es: " + cantidadPositivos + "<br>" + "4 - La cantidad de negativos es: " + cantidadNegativos + "<br>" + "5 - La cantidad de ceros es: " + cantidadCeros + "<br>" + "6 - La cantidad de números pares es: " + cantidadPares + "<br>" + "7 - El promedio de positivos es: " + promedioPositivos + "<br>" + "8 - El promedio de negativos es: " + promedioNegativos + "<br>" + " 9 - La diferencia entre los positivos y los negativos es: " + diferencia);
 }
+
+/* CHEQUEAR ESTOOOOOOOOOOOOOO
+
+do {
+		numeroIngresado = parseInt(prompt("Ingresar un numero positivo o negativo"));
+
+
+		if (numeroIngresado > 0) {
+			control2++
+			cantidadPositivos = control2;
+			sumaPositivos = sumaPositivos + numeroIngresado;
+			if(control2 == 0){
+				promedioPositivos = "No se puede dividir por 0";
+			}else{
+				promedioPositivos = sumaPositivos / control2;
+			}
+			
+
+			if (numeroIngresado % 2 == 0) {
+				control1++;
+				cantidadPares = control1;
+			}
+		} else if (numeroIngresado < 0) {
+			control3++
+			sumaNegativos = sumaNegativos + numeroIngresado;
+			cantidadNegativos = control3;
+
+			if(control3 == 0){
+				promedioNegativos = "No se puede dividir por 0";
+			}else{
+				promedioNegativos = sumaNegativos / control3;
+			}
+
+			if (numeroIngresado % 2 == 0) {
+				control1++;
+				cantidadPares = control1;
+			}
+		}
+
+		if (numeroIngresado == 0) {
+			control4++;
+			cantidadCeros = control4;
+		}
+
+		respuesta = prompt("desea continuar?");
+	} while (respuesta == "si");
+
+	diferencia = sumaPositivos - sumaNegativos;
+
+	document.write("1 - La suma de los negativos es: " + sumaNegativos + "<br>" + "2 - La suma de los positivos es: " + sumaPositivos + "<br>" + "3 - La cantidad de positivos es: " + cantidadPositivos + "<br>" + "4 - La cantidad de negativos es: " + cantidadNegativos + "<br>" + "5 - La cantidad de ceros es: " + cantidadCeros + "<br>" + "6 - La cantidad de números pares es: " + cantidadPares + "<br>" + "7 - El promedio de positivos es: " + promedioPositivos + "<br>" + "8 - El promedio de negativos es: " + promedioNegativos + "<br>" + " 9 - La diferencia entre los positivos y los negativos es: " + diferencia);*/
