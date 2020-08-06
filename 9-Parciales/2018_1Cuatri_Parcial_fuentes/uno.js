8b5903934c8290c59c6baa301a801c4603bb3d81
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-06 19:24:49 -0300

    resuelto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,10 @@
 
 function mostrar()
 {
+    let base = parseInt(prompt("Ingrese la base del rectangulo"));
+    let altura = parseInt(prompt("Ingrese la altura"));
+    let perimetro = (base*2)+(altura*2);
+
+    console.log("El perímetro del rectanculo es de "+perimetro+" cm.")
 
 }
