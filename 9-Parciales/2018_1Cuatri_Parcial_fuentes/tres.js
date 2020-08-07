0ed9550fdb436757099ce1971ff4a56ad0d7de7b
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-07 15:56:44 -0300

    3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,20 @@
-function mostrar()
-{
+function mostrar() {
+
+    let precio = prompt("Ingrese el precio");
+    while (isNaN(precio)) {
+        alert("Formato de precio invalido");
+        precio = prompt("Ingrese el precio");
+    }
+
+    let porcentaje = prompt("Ingrese un porcentaje (sin el %)");
+
+    while (isNaN(porcentaje)) {
+        alert("Formato de porcentaje invalido")
+        porcentaje = prompt("Ingrese un porcentaje (sin el %)");
+    }
+
+    let precioFinal = parseFloat(precio) - parseFloat((precio * porcentaje) / 100);
+
+    document.getElementById("elPrecioFinal").value = precioFinal;
 
 }
