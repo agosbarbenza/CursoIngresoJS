8a869bd178a9ec34a9693f438f3d03c5cbe4a9eb
w Agostina Barbenza <agosbarbenza@gmail.com>
2020-08-07 20:45:22 -0300

    resolucion

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,9 +1,18 @@
 function mostrar()
 {
-	let num = parseInt(prompt("Ingrese un numero"));
-	let i
+
+	//VOLVERLO A HACER DEL MODO QUE LO HIZO LA COMPAÑERA
+	
+	let num = prompt("Ingrese un numero");
+	while(num <= 1 || isNaN(num)){
+		alert("Numero incorrecto");
+		num = prompt("Ingrese un numero");
+	}
+
+	let i;
 	let bandera = 0;
 
+	
 	for (i = 2; i < num; i++){   //es primo si solo es divisible x 1 y x él mismo.
 		if(num%i == 0){
 			bandera = 1;
@@ -12,6 +21,8 @@ function mostrar()
 
 	if(bandera == 0){
 		alert("El numero "+num+" es primo");
+	}else{
+		alert("El numero "+num+" no es primo");
 	}
 
 }//FIN DE LA FUNCIÓN
